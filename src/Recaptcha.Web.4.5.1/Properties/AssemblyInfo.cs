1ee87e8963006da662b1238f808c4d60abd6441f
Martin Weiss <martin.weiss@downlo-dev.com>
2019-03-01 12:57:18 +0000

    Fixes #23 .NET 4.7.1 support (and VS15 settings..)

--- src/Recaptcha.Web.4.5.1/Properties/AssemblyInfo.cs
+++ src/Recaptcha.Web.4.5.1/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A library that allows a developer to easily integrate Google's free Recaptcha service (the most popular captcha control) with an ASP.NET Web Forms or ASP.NET MVC Web application.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Recpatcha for .NET")]
+[assembly: AssemblyProduct("Recaptcha for .NET")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
