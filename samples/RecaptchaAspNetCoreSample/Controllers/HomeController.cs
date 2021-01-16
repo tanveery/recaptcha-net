2af62a2af2590be2fe91a3f366dba9c4f9724003
Tanveer Yousuf <tanveery@users.noreply.github.com>
2021-01-16 12:54:43 +0530

    Added ASP.NET Core sample.

---
+++ samples/RecaptchaAspNetCoreSample/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿/* ============================================================================================================================
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * =========================================================================================================================== */
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Recaptcha.Web.Mvc;
+using RecaptchaAspNetCoreSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecaptchaAspNetCoreSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserRegistrationModel model)
+        {
+            var recaptchaHelper = this.GetRecaptchaVerificationHelper();
+
+            if (String.IsNullOrEmpty(recaptchaHelper.Response))
+            {
+                ModelState.AddModelError("", "Captcha answer cannot be empty.");
+                return View(model);
+            }
+
+            var recaptchaResult = recaptchaHelper.VerifyRecaptchaResponse();
+
+            if (!recaptchaResult.Success)
+            {
+                foreach (var err in recaptchaResult.ErrorCodes)
+                {
+                    ModelState.AddModelError("", err);
+                }
+            }
+            else
+            {
+                return RedirectToAction("Welcome");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ samples/RecaptchaAspNetCoreSample/Models/UserRegistrationModel.cs
@@ -0,0 +1,28 @@
+﻿/* ============================================================================================================================
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+ * LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * =========================================================================================================================== */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RecaptchaAspNetCoreSample.Models
+{
+    public class UserRegistrationModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+    }
+}
